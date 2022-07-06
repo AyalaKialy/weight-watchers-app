853138bbc20d3e0e7b686c9a5ac77f66217b0c95
AyalaKialy <ayalak4207@gmail.com>
2022-07-06 17:53:26 +0300

    start server side

---
+++ src/server/router/user.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/user.controller')
+
+router.get('/:id', controller.getUserById);
+router.get('/', controller.getAllUsers);
+router.post('/', controller.createUser);
+router.put('/:id', controller.updateUser);
+router.delete('/:id', controller.deleteUser)
+
+module.exports = router;
+
+
+
+
